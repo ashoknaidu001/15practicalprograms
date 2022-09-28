ade0a0863a5e1619c7a8280f401468eaeab89d7d
Ashok Naidu <112234401+ashoknaidu001@users.noreply.github.com>
2022-09-28 10:40:51 +0530

    Add files via upload

---
+++ maximumnumprogram.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float n1,n2;
+	cout<<"enter three numbers:";
+	cin>>n1>>n2;
+	if (n1>=n2) {
+		if (n1>=n2)
+		cout<<"maximum number:"<<n1;
+		else
+		cout<<"maximum number:"<<n2;
+    }
+    else {
+    	if(n2>=n1)
+    	   cout<<"maximum number:"<<n2;
+    	else
+    	   cout<<"maximum number:"<<n1;
+    }
+    return 0;
+}
+	
+	
+
